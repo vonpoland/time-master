12b6947ed3e07bb508a99d00f913b43cf2572699
marcin <marcinn.krawczyk@gmail.com>
2016-07-13 20:04:02 +0200

    improved tests

--- test/unit/event/addEvent.spec.js
+++ test/unit/event/addEvent.spec.js
@@ -43,7 +43,7 @@ describe('events tests', function () {
 
     it('should not throw error when TimeEvent is passed', function () {
         let timeMaster = new TimeMaster();
-        let timeEvent = TimeMaster.createEvent('event', new Date(), () => {});
+        let timeEvent = TimeMaster.createEvent('event', moment(), () => {});
         let wasError = false;
 
         try {
@@ -60,6 +60,16 @@ describe('events tests', function () {
         expect(timeMaster.events.length).to.be(0);
     });
 
+    it('Time event must be instance of moment', function () {
+        let wasError = false;
+        try {
+            TimeMaster.createEvent('test', new Date(), () => {})
+        } catch (error) {
+            wasError = true;
+        }
+
+        expect(wasError).to.be(true);
+    })
 
     it('should run event with given time', function (done) {
         let timeMaster = new TimeMaster();
@@ -74,11 +84,11 @@ describe('events tests', function () {
     it('should run events in sequence', function (done) {
         let timeMaster = new TimeMaster();
         let order = [];
-        let timeEvent1 = TimeMaster.createEvent('name1', moment().add(0.02, 'second'), () => order.push(timeEvent1.name));
-        let timeEvent2 = TimeMaster.createEvent('name2', moment().add(0.03, 'second'), () => order.push(timeEvent2.name));
-        let timeEvent3 = TimeMaster.createEvent('name3', moment().add(0.04, 'second'), () => order.push(timeEvent3.name));
-        let timeEvent4 = TimeMaster.createEvent('name4', moment().add(0.05, 'second'), () => order.push(timeEvent4.name));
-        let timeEvent5 = TimeMaster.createEvent('name5', moment().add(0.06, 'second'), () => order.push(timeEvent5.name));
+        let timeEvent1 = TimeMaster.createEvent('name1', moment().add(0.002, 'second'), () => order.push(timeEvent1.name));
+        let timeEvent2 = TimeMaster.createEvent('name2', moment().add(0.003, 'second'), () => order.push(timeEvent2.name));
+        let timeEvent3 = TimeMaster.createEvent('name3', moment().add(0.004, 'second'), () => order.push(timeEvent3.name));
+        let timeEvent4 = TimeMaster.createEvent('name4', moment().add(0.005, 'second'), () => order.push(timeEvent4.name));
+        let timeEvent5 = TimeMaster.createEvent('name5', moment().add(0.006, 'second'), () => order.push(timeEvent5.name));
         let events = shuffle([timeEvent1, timeEvent2, timeEvent3, timeEvent4, timeEvent5]);
 
         events.forEach(event => timeMaster.addEvent(event));
@@ -90,6 +100,22 @@ describe('events tests', function () {
             expect(order[3]).to.be('name4');
             expect(order[4]).to.be('name5');
             done();
-        }, 100);
+        }, 10);
+    });
+
+    it('should run when they have same time', function (done) {
+        let timeMaster = new TimeMaster();
+        let order = [];
+        let time = moment().add(0.02, 'second');
+        let timeEvent1 = TimeMaster.createEvent('name1', time, () => order.push(timeEvent1.name));
+        let timeEvent2 = TimeMaster.createEvent('name2', time, () => order.push(timeEvent2.name));
+        let events = shuffle([timeEvent1, timeEvent2]);
+
+        events.forEach(event => timeMaster.addEvent(event));
+
+        setTimeout(() => {
+            expect(order.length).to.be(2);
+            done();
+        }, 25);
     });
 });
